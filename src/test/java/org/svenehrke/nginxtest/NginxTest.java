b958f1358a752d348fc4c3ba70941a9a034746af
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2022-02-13 08:41:27 +0100

    specify custom config

--- src/test/java/org/svenehrke/nginxtest/NginxTest.java
+++ src/test/java/org/svenehrke/nginxtest/NginxTest.java
@@ -13,19 +13,53 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class NginxTest {
 
 	private static final DockerImageName NGINX_IMAGE = DockerImageName.parse("nginx:1.21.6");
+	public static final String CONTAINER_NAME = "nginx-tc";
 
 	static File htmlFolder;
+	static File configFolder;
 	static NginxContainer<?> container;
 
 	private static final String NGINX_CONTAINER_HTML_FOLDER = "/usr/share/nginx/html/";
+	private static final String NGINX_CONTAINER_CONFIG_FILE = "/etc/nginx/nginx.conf";
+	private static final String NGINX_CONTAINER_CONFIG_DIR = "/etc/nginx/nginx.conf/conf.d";
 
-	private static final String NGINX_CONFIG_TEMPLATE = """
+	private static final String NGINX_CONTAINER_CONFIG_FILE_TEMPLATE = """
+		user  nginx;
+		worker_processes  auto;
+		
+		error_log  /var/log/nginx/error.log notice;
+		pid        /var/run/nginx.pid;
+		
+		events {
+			worker_connections  1024;
+		}
+		
+		http {
+			include       /etc/nginx/mime.types;
+			default_type  application/octet-stream;
+		
+			log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
+							  '$status $body_bytes_sent "$http_referer" '
+							  '"$http_user_agent" "$http_x_forwarded_for"';
+		
+			access_log  /var/log/nginx/access.log  main;
+		
+			sendfile        on;
+			#tcp_nopush     on;
+		
+			keepalive_timeout  65;
+		
+			#gzip  on;
+		
+			include /etc/nginx/conf.d/*.conf;
+		}
 		""";
 	private static final String INDEX_HTML = """
   			<html><body>Hello World!</body></html>
@@ -34,11 +68,16 @@ public class NginxTest {
 	@BeforeAll
 	static void setUp() throws IOException {
 		htmlFolder = TestUtil.createTempFolder("testcontainer_nginx_html_folder");
-		File indexHtmlFile = TestUtil.newTempFile(htmlFolder, "index.html");
-		Files.writeString(indexHtmlFile.toPath(), INDEX_HTML);
+		Files.writeString(TestUtil.newTempFile(htmlFolder, "index.html").toPath(), INDEX_HTML);
+
+		configFolder = TestUtil.createTempFolder("testcontainer_nginx_config_folder");
+		File configFile = TestUtil.newTempFile(configFolder, "default.conf");
+		Files.writeString(configFile.toPath(), NGINX_CONTAINER_CONFIG_FILE_TEMPLATE);
 
 		container = new NginxContainer<>(NGINX_IMAGE)
+			.withCreateContainerCmdModifier(cmd -> cmd.withName(CONTAINER_NAME)) // use this line just for development! (can cause name clashes)
 			.withCopyFileToContainer(MountableFile.forHostPath(htmlFolder.toPath()), NGINX_CONTAINER_HTML_FOLDER)
+			.withCopyFileToContainer(MountableFile.forHostPath(configFile.toPath()), NGINX_CONTAINER_CONFIG_FILE)
 			.waitingFor(new HttpWaitStrategy())
 		;
 		container.start();
